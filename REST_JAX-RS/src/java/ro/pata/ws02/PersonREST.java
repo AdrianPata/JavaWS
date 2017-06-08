3d525309ed7421786bace5dbd396bc328ac8d275
Adrian <ohamoha@gmail.com>
2017-06-08 11:54:21 +0300

    Start adding CRUD to JAX-RS.

--- REST_JAX-RS/src/java/ro/pata/ws02/PersonREST.java
+++ REST_JAX-RS/src/java/ro/pata/ws02/PersonREST.java
@@ -8,9 +8,11 @@ package ro.pata.ws02;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -19,11 +21,29 @@ import javax.ws.rs.core.MediaType;
 @Path("/")
 public class PersonREST {
     @Context ServletContext ctx;
+    private static Persons agenda;
+    
+    private void loadAgenda(){
+        if(agenda==null){
+            System.out.println("******** INIT Agenda ****************");
+            agenda=new Persons(ctx);
+            agenda.setAccept("xml");
+        }
+    }
     
     @GET
     @Produces(MediaType.APPLICATION_XML)
     @Path("/xml")
     public String getTest(){
-        return "<msg>Test ok</msg>";
+        loadAgenda();
+        return agenda.getAgenda();
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    @Path("/xml/{id: \\d+}")
+    public Response getPerson(@PathParam("id") int id){
+        //return Response.status(Response.Status.FORBIDDEN).type(MediaType.TEXT_PLAIN).build();
+        return Response.ok("Salut "+id,MediaType.TEXT_PLAIN).build();
     }
 }
